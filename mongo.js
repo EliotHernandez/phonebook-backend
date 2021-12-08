8213235b4860811edb3dcbaa60853d6835978fc2
Eliot <eliot@artek.es>
2021-12-08 20:11:53 +0000

    examples for mongodb persistence

--- mongo.js
+++ mongo.js
@@ -29,7 +29,7 @@ const Person = mongoose.model('Person', personSchema)
 if (action === 'get') {
     Person.find({}).then(result => {
         result.forEach(person => {
-            console.log(person)
+            console.log(person.name + ' ' + person.number)
         })
         mongoose.connection.close()
     })
